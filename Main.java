fd80375f59ed369f5a93e11cd90c922ba4dc9446
ethanpayne1 <payne.ethan6@yahoo.com>
2023-11-25 22:58:17 -0600

    Route method is finished but breaks with map func

--- Main.java
+++ Main.java
@@ -38,9 +38,10 @@ public class Main {
 
 
     //-- Helper method to get the weight between two vertices
-    private static int getWeight(Graph<String> graph, String vertex1, String vertex2) {
-        for (Graph.Edge<String> edge : graph.neighbors.get(graph.vertices.indexOf(vertex1))) {
-            if (edge.targetVertex.equals(vertex2)) {
+    private static int getWeight(Graph<String> graph, String firstVertex, String secondVertex ) {
+
+        for (Graph.Edge<String> edge : graph.neighbors.get(graph.vertices.indexOf(firstVertex))) {
+            if (edge.targetVertex.equals(secondVertex)) {
                 return edge.weight;
             }
         }
@@ -82,7 +83,7 @@ public class Main {
                 String[] parts = line.split(" ");
                 String vertex = parts[0];
                 graph.addVertex(vertex);
-
+                //-- Hoth Endor,9 Coruscant,3 Tatooine,1 Dagobah,5
                 for (int i = 1; i < parts.length; i++) {
                     String[] edge = parts[i].split(",");
                     String adjVertex = edge[0];
@@ -105,6 +106,60 @@ public class Main {
             e.printStackTrace();
         } in.close();
     } 
+
+    /*
+    * Route File Format: 
+    -- 1) Each line in the file will contain the pilot's name followed by a list of planets
+    -- 2) Each planet will be seperated by a space
+    -- 3) The first vertex listed is the starting vertex of the patrol
+    --      Example: Sully_Olvar Tatooine Dagobah Naboo Yavin Alderaan
+    -- 
+    */
+
+    public static List<String> readRoute() {
+        List<String> pRoute = new ArrayList<>();
+        Graph<String> graph = new Graph<>();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the name of the route file"); 
+        String fileString = in.nextLine();
+
+        try {
+            File file = new File(fileString);
+            Scanner fileScan = new Scanner(file);
+
+            while (fileScan.hasNextLine()) {
+                String line = fileScan.nextLine();
+                String[] parts = line.split(" ");
+                String name = parts[0];
+                String startingVertex = parts[1];
+                graph.addVertex(startingVertex);
+                //String path = parts[2].substring(2);
+                
+                for (int i = 2; i < parts.length; i++) {
+                    graph.addVertex(parts[i]);
+                }
+                pRoute.add(line);
+                System.out.println("route info test: " + line);
+                System.out.println("name test" + name);
+                System.out.println("start vertex test" + startingVertex);
+            } fileScan.close();
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("Route file doesnt exist");
+            e.printStackTrace();
+        } in.close();
+        return pRoute;
+    }
+    
+    
+    
+    /*StringTokenizer token = new StringTokenizer(line, " ");
+    String name = token.nextToken();
+    String startingVertex = token.nextToken();
+    String planets = token.nextToken(" ");
+    System.out.println("name: " + "starting vertex: " + "planets: "); */
+    //pRoute.add(new List(name,startingVertex,planets)); 
+    
 }
 
 /*Scanner fileReader = new Scanner(new File("Galaxy_Map.txt"));
